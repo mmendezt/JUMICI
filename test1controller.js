ba3f8c53d0544bb8f116fcdbb76282bb68571243
mmendezt <m.mendezt@alumnos.upm.es>
2021-05-27 19:50:45 +0200

    Made displaying text

--- test1controller.js
+++ test1controller.js
@@ -7,6 +7,8 @@ const toggleOut = (position) => {
     if(document.getElementById(position).classList.contains('center'))
         document.getElementById(position).classList.remove('center');
 };
+
+
 const toggleCenter = (position) => {
     if ( document.getElementById(position).classList.contains('center') )
         document.getElementById(position).classList.toggle('center');
@@ -15,4 +17,54 @@ const toggleCenter = (position) => {
     //If the character was out remove that class, since it is no longer out of the scene 
     if(document.getElementById(position).classList.contains('out'))
         document.getElementById(position).classList.remove('out');
+};
+$( run )
+var textTimer, fullText, currentOffset;
+
+function run(){
+    toggleCenter("left");
+    Speak("Peter","Hey! IT guy wait!. My <strong style='color: Red;'>internet <i>connection<i> </strong> has failed, SOLVE IT! aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+}
+
+function Speak(person, text) {
+    $("#name").html(person);
+    console.log(person);
+    console.log($("#name").html());
+    fullText = text;
+    currentOffset = 0;
+    textTimer = setInterval(onTick, 100);
+    $(".dialog").click(function(){ complete();});
+}
+
+function onTick() {
+    currentOffset++;
+
+    if(fullText[currentOffset]=="<"){
+        while(fullText[currentOffset]!=">"){
+            currentOffset++;
+        }
+    }
+
+
+    if (currentOffset == fullText.length) {
+        complete();
+        return;
+    }
+    var text = fullText.substring(0, currentOffset);
+    $("#message").html(text);
+}
+
+const complete = () => {
+    clearInterval(textTimer);
+    textTimer = null;
+    $("#message").html(fullText);
+    $("#name").html("Peter");
+}
+
+
+
+
+const changeHTMLbyID = (id,content) =>{
+    console.log($(id).html());
+    $(id).html(content);
 };
